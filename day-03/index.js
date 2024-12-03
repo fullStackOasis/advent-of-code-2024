771030307395530624b2a083461ced4b3fb730a6
fullStackOasis <fullstackdev@fullstackoasis.com>
2024-12-03 13:13:25 -0500

    Advent of Code Day 3 complete.

--- day-03/index.js
+++ day-03/index.js
@@ -47,17 +47,22 @@ const readData = async (fileName) => {
  * Use a regex to get matches to mul(n,n) where n is an integer with digits
  * 1-9 and no longer than 3 in length.
  * @param {*} input a string
+ * @param {*} example number, either 1 or 2
+ * 
  * returns an array of matches like [ 'mul(2,4)', 'mul(5,5)'... ]
  */
-const findMatches = (input) => {
-  const regex = /mul\(\d{1,3},\d{1,3}\)/g; // thank you ChatGPT
+const findMatches = (input, exampleNumber) => {
+  const regex = exampleNumber == 2 ?
+    /mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)/g :
+    /mul\(\d{1,3},\d{1,3}\)/g; // thank you ChatGPT
   const matches = input.match(regex);
   return matches;
 };
 
 /**
  * Loop over the matches to get the integers, multiply them, and add.
- * @param {*} matches 
+ * @param {*} matches
+ * returns the summed result
  */
 const multiplyMatchesThenAdd = (matches) => {
   const mul = (a, b) => { return a * b; };
@@ -68,16 +73,51 @@ const multiplyMatchesThenAdd = (matches) => {
   });
   const total = results.reduce((sum, current) => sum + current, 0);
   return total;
-}
+};
+
+/**
+ * Loop over the matches to get the integers, as well as any do or don't
+ * instructions. As needed, multiply them, then add.
+ *
+ * @param {*} matches
+ * returns the result
+ */
+const doDontMultiplyMatchesThenAdd = (matches) => {
+  const mul = (a, b) => { return a * b; };
+  let doOperation = true;
+  const results = matches.map(match => {
+    // Extract the numbers from the match
+    const [, a, b] = match.match(/mul\((\d{1,3}),(\d{1,3})\)/) || [];
+    if (a && b) {
+      return doOperation ? mul(Number(a), Number(b)) : 0;
+    } else {
+      const dont = match.match(/don't\(\)/);
+      const doit = match.match(/do\(\)/);
+      if (dont) {
+        doOperation = false;
+      } else if (doit) {
+        doOperation = true;
+      } else {
+        // Help debug, but should not get here.
+        console.log(`dont: ${dont} doit: ${doit} match: ${match} ${a} ${b}`);
+        throw new Error(`invalid match`);
+      }
+      return 0;
+    }
+  });
+  const total = results.reduce((sum, current) => sum + current, 0);
+  return total;
+};
 
 const main = async (fileName) => {
+  const { corrupted } = await readData(fileName);
+  const matches = findMatches(corrupted, Number(part));
   if (part == "1") {
-    const { corrupted } = await readData(fileName);
-    const matches = findMatches(corrupted);
     const total = multiplyMatchesThenAdd(matches);
-    console.log(`total: ${total}`);
+    console.log(`total part 1: ${total}`);
   } else if (part == "2") {
-    throw new Error(`incomplete`);
+    const total = doDontMultiplyMatchesThenAdd(matches);
+    console.log(`total part 2: ${total}`);
   }
 };
 
