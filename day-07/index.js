64dacf772bd102560fe14ea2581ef2c7f27954f4
fullStackOasis <fullstackdev@fullstackoasis.com>
2024-12-07 07:43:38 -0500

    Advent of Code Day 7. Refactor input code into a library and use it in new day 7 code.

--- day-07/index.js
+++ day-07/index.js
@@ -7,24 +7,9 @@
  */
 const fs = require("fs").promises;
 
-const fileName = process.argv[2];
+const { validateInput } = require("../lib");
 
-if (!fileName) {
-  console.error(
-    "Error: Please provide a file name as a command-line argument."
-  );
-  process.exit(1); // Exit with error code
-}
-
-// Part is a number, 1 or 2, meaning the "part" of the exercise to be completed.
-const part = process.argv[3];
-
-if (part != "1" && part != "2") {
-  console.error(
-    "Error: Please provide part (1 or 2) as a command-line argument."
-  );
-  process.exit(1); // Exit with error code
-}
+const {fileName, part} = validateInput();
 
 const main = async (fileName) => {
   throw new Error(`incomplete`);
