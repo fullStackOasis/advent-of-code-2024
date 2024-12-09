908c2ed3f5afcfa6fe1b1bc211820cf897f665d7
fullStackOasis <fullstackdev@fullstackoasis.com>
2024-12-09 13:02:57 -0500

    Advent of Code Day 9. Implement algorithm for converting processed to compressed.

--- day-09/index.js
+++ day-09/index.js
@@ -23,39 +23,83 @@ const readData = async (fileName) => {
   }
 };
 
+/**
+ * Swaps a dot in the middle of the raw array with the input character.
+ *
+ * @param {*} raw the array of characters
+ * @param {*} ch character to swap
+ * @param {*} chPos character's original position
+ */
+const swapFirstDotWith = (raw, ch, chPos) => {
+  const pos = raw.indexOf(".");
+  raw[pos] = ch;
+  raw[chPos] = ".";
+};
+
+/**
+ * Turns the raw string like 2333133121414131402 to the compressed string, like
+ * 0099811188827773336446555566..............
+ * @param {*} raw
+ * @returns
+ */
+const convertRawToCompressed = (raw) => {
+  const DEBUG = false;
+  // It is easy programmatically to manipulate this string if we split it into
+  // an array of characters. You cannot mutate the string.
+  const rawArray = raw.split("");
+  // Get count of all elements that are "."
+  let nDots = rawArray.filter((el) => el == ".").length;
+  const len = rawArray.length;
+  for (let i = len - 1; i > -1; i--) {
+    const ch = rawArray[i];
+    swapFirstDotWith(rawArray, ch, i);
+    nDots--;
+    if (nDots == 0) {
+      break;
+    }
+    if (DEBUG) process.stdout.write(ch);
+  }
+  if (DEBUG) console.log("");
+  return rawArray.join("");
+};
+
+/**
+ * Turns the input line into the raw data like "0..111....22222"
+ * @param {*} line
+ * @returns
+ */
 const convertLineToRaw = (line) => {
-  console.log(line);
+  const DEBUG = false;
+  if (DEBUG) console.log(line);
   const len = line.length;
   let idNumber = 0;
   let result = "";
   for (let i = 0; i < len; i++) {
     const num = line[i];
     const isFreeSpace = i % 2 == 1;
-    //console.log(`num, isFreeSpace ${num} ${isFreeSpace}`);
     for (let j = 0; j < num; j++) {
       const ch = isFreeSpace ? "." : idNumber + "";
       result += ch;
-      process.stdout.write(ch);
+      if (DEBUG) process.stdout.write(ch);
     }
     if (isFreeSpace) {
       idNumber++;
     }
   }
-  console.log("");
+  if (DEBUG) console.log("");
   return result;
 };
 
 const main = async (fileName) => {
   // Single line expected.
   const { line } = await readData(fileName);
-  console.log(line);
   if (part == "1") {
     const raw = convertLineToRaw(line);
-    console.log(raw);
+    const compressed = convertRawToCompressed(raw);
+    console.log(`compressed: ${compressed}`);
   } else if (part == "2") {
+    throw new Error(`unfinished`);
   }
-  throw new Error(`unfinished`);
-
 };
 
 main(fileName);
